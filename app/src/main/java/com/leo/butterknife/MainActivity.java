e9346e5e04d40c16ceda29bdc67ce505071d39d2
Leo冯 <2549732107@qq.com>
2019-02-26 10:52:32 +0800

    666

---
+++ app/src/main/java/com/leo/butterknife/MainActivity.java
@@ -0,0 +1,54 @@
+package com.leo.butterknife;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button1)
+    Button button1;
+    @BindView(R.id.button2)
+    Button button2;
+    @BindView(R.id.button3)
+    Button button3;
+    @BindView(R.id.tv_main)
+    TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        EventBus.getDefault().register(this);
+    }
+
+    @OnClick(R.id.button1)
+    public void showTo() {
+        startActivity(new Intent(this, Main2Activity.class));
+        Toast.makeText(this, "点击了按钮1", Toast.LENGTH_SHORT).show();
+    }
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void Event(Base base){
+        tvMain.setText(base.getMsg());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)){
+            EventBus.getDefault().unregister(this);
+        }
+    }
+}
